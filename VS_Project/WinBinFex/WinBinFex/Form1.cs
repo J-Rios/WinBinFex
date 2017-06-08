872bd23845d26361020b28efcf920b84cabf0164
J-Rios <jose_23991@hotmail.com>
2017-06-08 15:16:06 +0200

    Added project sources and release (ver 1.0)

---
+++ VS_Project/WinBinFex/WinBinFex/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WinBinFex
+{
+    public partial class Form1 : Form
+    {
+        /******************************************************************************/
+        // Objects, Types, Constants, Variables
+
+        Thread oThread;
+
+        enum fileType { BIN, FEX, OTHER };
+
+        fileType file_type;
+
+        /******************************************************************************/
+        // Form creation
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Normal;
+            this.Show();
+            this.BringToFront();
+
+            file_type = fileType.OTHER;
+            label_status.ForeColor = System.Drawing.Color.Black;
+            label_status.Text = "Status: Please, insert a BIN or FEX file";
+        }
+
+        /******************************************************************************/
+        // Elements events
+
+        private void button_fileExplorer_OnClick(object sender, EventArgs e)
+        {
+            openFileDialog.Title = "Select a BIN or FEX file";
+            openFileDialog.Filter = "All Files|*.*|BIN File|*.bin|FEX File|*.fex";
+            openFileDialog.FileName = Path.GetFileName(textBox_file_path.Text);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                button_explorer.Enabled = false;
+                textBox_file_path.Text = openFileDialog.FileName;
+            }
+            button_explorer.Enabled = true;
+        }
+
+        private void button_convert_OnClick(object sender, EventArgs e)
+        {
+            string inputFile = textBox_file_path.Text;
+
+            if (String.IsNullOrEmpty(inputFile))
+            {
+                label_status.ForeColor = System.Drawing.Color.Red;
+                label_status.Text = "Status: The file path is empty";
+            }
+            else if (!File.Exists(inputFile))
+            {
+                label_status.ForeColor = System.Drawing.Color.Red;
+                label_status.Text = "Status: The file does not exist";
+            }
+            else if (!is_BINFEX_file(inputFile))
+            {
+                label_status.ForeColor = System.Drawing.Color.Red;
+                label_status.Text = "Status: Not a valid BIN or FEX file";
+            }
+            else
+            {
+                button_convert.Enabled = false;
+                label_status.ForeColor = System.Drawing.Color.Black;
+                label_status.Text = "Status: Converting...";
+
+                oThread = new Thread(new ThreadStart(process));
+                oThread.SetApartmentState(ApartmentState.STA);
+                oThread.IsBackground = true;
+                oThread.Start();
+            }
+        }
+
+        /******************************************************************************/
+        // Convertion Process (Thread)
+
+        [STAThread] private void process()
+        {
+            string filePath = "";
+            string exportPath = "";
+
+            filePath = textBox_file_path.Text;
+
+            if (file_type == fileType.BIN)
+            {
+                saveFileDialog.Title = "Save location for generated FEX";
+                saveFileDialog.Filter = "FEX File|*.fex";
+                saveFileDialog.FileName = "script.fex";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    exportPath = saveFileDialog.FileName;
+
+                systemCall("bin\\fexc -I bin -O fex " + filePath + " " + exportPath);
+            }
+            else if (file_type == fileType.FEX)
+            {
+                saveFileDialog.Title = "Save location for generated BIN";
+                saveFileDialog.Filter = "BIN File|*.bin";
+                saveFileDialog.FileName = "script.bin";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    exportPath = saveFileDialog.FileName;
+
+                systemCall("bin\\fexc -I fex -O bin " + filePath + " " + exportPath);
+            }
+        }
+
+        /******************************************************************************/
+        // Functions
+
+        private void systemCall(string cmd)
+        {
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.UseShellExecute = false;
+                startInfo.CreateNoWindow = true;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C " + cmd;
+                process.StartInfo = startInfo;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    label_status.Invoke((Action)(() =>
+                    {
+                        MessageBox.Show("The file cannot be converted.\n\nError:\n\n" + error);
+                        label_status.ForeColor = System.Drawing.Color.Red;
+                        label_status.Text = "Status: Error, the file cannot be converted (malformed file)";
+                    }));
+                }
+                else
+                {
+                    label_status.Invoke((Action)(() =>
+                    {
+                        label_status.ForeColor = System.Drawing.Color.ForestGreen;
+                        label_status.Text = "Status: Success";
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The file cannot be converted.\n\nError:\n\n" + ex.ToString());
+            }
+
+            button_convert.Invoke((Action)(() =>
+            {
+                button_convert.Enabled = true;
+            }));
+        }
+        
+        private bool is_BIN_file(string file)
+        {
+            char[] extension = new char[4];
+
+            extension[0] = file[file.Length - 4];
+            extension[1] = file[file.Length - 3];
+            extension[2] = file[file.Length - 2];
+            extension[3] = file[file.Length - 1];
+
+            if ((extension[0] == '.') && (extension[1] == 'b' || extension[1] == 'B') && (extension[2] == 'i' || extension[2] == 'I') && (extension[3] == 'n' || extension[3] == 'N'))
+            {
+                file_type = fileType.BIN;
+                return true;
+            }
+            else
+            {
+                file_type = fileType.OTHER;
+                return false;
+            }
+        }
+
+        private bool is_FEX_file(string file)
+        {
+            char[] extension = new char[4];
+
+            extension[0] = file[file.Length - 4];
+            extension[1] = file[file.Length - 3];
+            extension[2] = file[file.Length - 2];
+            extension[3] = file[file.Length - 1];
+
+            if ((extension[0] == '.') && (extension[1] == 'f' || extension[1] == 'F') && (extension[2] == 'e' || extension[2] == 'E') && (extension[3] == 'x' || extension[3] == 'X'))
+            {
+                file_type = fileType.FEX;
+                return true;
+            }
+            else
+            {
+                file_type = fileType.OTHER;
+                return false;
+            }
+        }
+
+        private bool is_BINFEX_file(string file)
+        {
+            if (is_BIN_file(file) || is_FEX_file(file))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ VS_Project/WinBinFex/WinBinFex/Form1.designer.cs
@@ -0,0 +1,150 @@
+﻿namespace WinBinFex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_convert = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_file_path = new System.Windows.Forms.TextBox();
+            this.button_explorer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_status = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(270, 188);
+            this.button_convert.Margin = new System.Windows.Forms.Padding(4);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(103, 51);
+            this.button_convert.TabIndex = 0;
+            this.button_convert.Text = "Convert";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_OnClick);
+            // 
+            // textBox_file_path
+            // 
+            this.textBox_file_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_file_path.Location = new System.Drawing.Point(32, 135);
+            this.textBox_file_path.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_file_path.Name = "textBox_file_path";
+            this.textBox_file_path.Size = new System.Drawing.Size(527, 24);
+            this.textBox_file_path.TabIndex = 1;
+            // 
+            // button_explorer
+            // 
+            this.button_explorer.Location = new System.Drawing.Point(567, 135);
+            this.button_explorer.Margin = new System.Windows.Forms.Padding(4);
+            this.button_explorer.Name = "button_explorer";
+            this.button_explorer.Size = new System.Drawing.Size(45, 24);
+            this.button_explorer.TabIndex = 2;
+            this.button_explorer.Text = "...";
+            this.button_explorer.UseVisualStyleBackColor = true;
+            this.button_explorer.Click += new System.EventHandler(this.button_fileExplorer_OnClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 104);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Specify an input BIN or FEX file:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 37);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(504, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Application that convert BIN to Fex and FEX to BIN files of Allwinner Sunxi SoCs";
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(29, 296);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(56, 17);
+            this.label_status.TabIndex = 14;
+            this.label_status.Text = "Status: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(553, 296);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Version 1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 326);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_explorer);
+            this.Controls.Add(this.textBox_file_path);
+            this.Controls.Add(this.button_convert);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(681, 373);
+            this.MinimumSize = new System.Drawing.Size(681, 373);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sunxi Tools for Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBox_file_path;
+        private System.Windows.Forms.Button button_explorer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
